1ff759c0ffd5fe2610f55405600505670955c20f
Joctan Silva <joctan.silva@trinusco.com.br>
2024-07-28 10:20:53 -0300

    day 1

---
+++ day1.py
@@ -0,0 +1,14 @@
+flourQuantity = 500
+yeastQuantity = 10
+waterQuantity = 300
+doughTime = 10
+saltQuantity = 3
+riseTime = 2
+bakeTime = 30
+bakeTemperature = 200
+
+print("1. Mix {}g of Flour, {}g Yeast and {}ml Water in a bowl.".format(flourQuantity, yeastQuantity, waterQuantity))
+print("2. Knead the dough for {} minutes.".format(doughTime))
+print("3. Add {}g of Salt.".format(saltQuantity))
+print("4. Leave to rise for {} hours.".format(riseTime))
+print("5. Bake at {} degrees C for {} minutes.".format(bakeTemperature, bakeTime))
