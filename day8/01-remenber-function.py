5e4b33787eb58cad1689703770644c416b3d5ad7
Joctan <joctannsilva@gmail.com>
2024-11-13 11:10:46 -0300

    day 8 part 2

--- day8/01-remenber-function.py
+++ day8/01-remenber-function.py
@@ -14,3 +14,17 @@ def greet_with_name(name):
     print(f"Goodbye {name}")
 
 greet_with_name("Tomek")
+
+
+## TWO PARAMS
+def greet_two_params(name, location):
+    print(f"Hello {name} from {location}")
+
+greet_two_params("Tomek", "Brazil")
+
+
+## KEYWORD ARGUMENTS
+def greet_two_params(name, location):
+    print(f"Hello {name} from {location}")
+
+greet_two_params(name="Tomek", location="Brazil")
