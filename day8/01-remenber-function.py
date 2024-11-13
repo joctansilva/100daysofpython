28c6fa22291d10ed235e2de8dee8c5d707a631f8
Joctan <joctannsilva@gmail.com>
2024-11-12 23:43:27 -0300

    day 8 pt 1

---
+++ day8/01-remenber-function.py
@@ -0,0 +1,16 @@
+def gret():
+    print("Hello")
+    print("Bye")
+    print("Goodbye")
+
+gret()
+
+
+# USING PROPS
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"Bye {name}")
+    print(f"Goodbye {name}")
+
+greet_with_name("Tomek")
---
+++ day8/02-remaining-life-week.py
@@ -0,0 +1,6 @@
+def life_in_weeks(age):
+    remainingAge = 90 - age
+    ageInWeek = remainingAge * 52
+    print(f"You have {ageInWeek} weeks left.")
+    
+life_in_weeks(20)
